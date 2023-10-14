1a0650768508353a80cfeedded8feeda6cfd1686
muhammadgireev <146732797+muhammadgireev@users.noreply.github.com>
2023-10-14 19:42:18 +0300

    Add files via upload

---
+++ JavaScript/main.js
@@ -0,0 +1,63 @@
+let gameName;
+let descriptionGame;
+let gameVersion;
+let mainCharacterGame;
+let nameGameDeveloper;
+let versionlang;
+let mapName;
+let gameDeveloperWebsite;
+let programmLang;
+let interfaceLanguage;
+
+const nameFavoriteMovie = "Avengers";
+
+const nameApplicationMostTime = "youtube";
+
+const articleAboutJs = "https://ru.wikipedia.org/wiki/JavaScript";
+
+const loremIpsum =
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+const yourAge = 17;
+
+const yourYear = 2005;
+
+const dateBirthNotZero = 2;
+
+const numberBrothers = 3;
+
+const numberSisters = 3;
+
+const numberPeopleFamily = 8;
+
+const costTravelPlaceStudy = 80;
+
+const currentYear = 2023;
+
+let dollarExchangeRate = 97.31;
+
+let EuroExchangeRate = 102.55;
+
+let bitcoinExchangeRate = 26933;
+
+const currentYearMinusYearBirth = currentYear - yourYear;
+
+const currentYearMinusYourAge = currentYear - yourAge;
+
+const numberBrothersPlusNumberSisters = numberBrothers + numberSisters;
+
+const euroExchangeRateMultipliedBy1000 = EuroExchangeRate * 1000;
+
+const euroExchangeRateMultipliedBy25 = EuroExchangeRate * 2.5;
+
+const bitcoinExchangeRateMultipliedBy10000 = bitcoinExchangeRate * 10000;
+
+const costTravelPlaceStudyDividedByDollarRate =
+  costTravelPlaceStudy / dollarExchangeRate;
+
+const numberPeopleFamilyMinusNumberBrothersMinusNumberSisters =
+  numberPeopleFamily - numberBrothers - numberSisters;
+
+const zeroDividedByYourAge = 0 / yourAge;
+
+const thirtyFiveDividedByYourAge = 35 - yourAge;
